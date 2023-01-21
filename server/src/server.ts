401c0075cfccc74c3d72a50ee01c8aa4c12e7632
Artur <arturbr@gmail.com>
2023-01-21 12:38:40 -0300

    DB and route for habits

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,30 @@
-console.log("hi");
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import { PrismaClient } from '@prisma/client'
+
+const app = Fastify();
+const prisma = new PrismaClient();
+
+app.register(cors);
+
+app.get('/', (req, res) => {
+  return 'Hello World!'
+});
+
+app.get('/habits', async (req, res) => {
+  const habits = await prisma.habit.findMany({
+    where: {
+      title: {
+        startsWith: 'Beber',
+      }
+    }
+  });
+
+  return habits;
+});
+
+app.listen({
+  port: 3333,
+}).then(() => {
+  console.log('listening on port 3333')
+});
