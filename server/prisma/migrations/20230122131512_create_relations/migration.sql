9481181a66bb893f0dc3a64ace506116f1a03754
Artur <arturbr@gmail.com>
2023-01-22 10:52:51 -0300

    Remaining DB configuration and tables

---
+++ server/prisma/migrations/20230122131512_create_relations/migration.sql
@@ -0,0 +1,31 @@
+/*
+  Warnings:
+
+  - The required column `id` was added to the `day_habits` table with a prisma-level default value. This is not possible if the table is not empty. Please add this column as optional, then populate it before making it required.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_day_habits" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "day_id" TEXT NOT NULL,
+    "habit_id" TEXT NOT NULL,
+    CONSTRAINT "day_habits_day_id_fkey" FOREIGN KEY ("day_id") REFERENCES "days" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "day_habits_habit_id_fkey" FOREIGN KEY ("habit_id") REFERENCES "habits" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_day_habits" ("day_id", "habit_id") SELECT "day_id", "habit_id" FROM "day_habits";
+DROP TABLE "day_habits";
+ALTER TABLE "new_day_habits" RENAME TO "day_habits";
+CREATE UNIQUE INDEX "day_habits_day_id_habit_id_key" ON "day_habits"("day_id", "habit_id");
+CREATE TABLE "new_habit_week_days" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "habit_id" TEXT NOT NULL,
+    "week_day" INTEGER NOT NULL,
+    CONSTRAINT "habit_week_days_habit_id_fkey" FOREIGN KEY ("habit_id") REFERENCES "habits" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_habit_week_days" ("habit_id", "id", "week_day") SELECT "habit_id", "id", "week_day" FROM "habit_week_days";
+DROP TABLE "habit_week_days";
+ALTER TABLE "new_habit_week_days" RENAME TO "habit_week_days";
+CREATE UNIQUE INDEX "habit_week_days_habit_id_week_day_key" ON "habit_week_days"("habit_id", "week_day");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
